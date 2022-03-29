6f7bd15e8a88d27650693a5314b8d723e1e6e0b5
anurak <anurak@everapp.io>
2022-03-30 00:00:30 +0700

    Reverse

--- src/TwilioVideoLocalView.android.js
+++ src/TwilioVideoLocalView.android.js
@@ -1,41 +1,34 @@
-//
-//  TwilioVideoLocalView.js
-//  Black
-//
-//  Created by Martín Fernández on 6/13/17.
-//
-//
+/**
+ * Component for Twilio Video local views.
+ *
+ * Authors:
+ *   Jonathan Chang <slycoder@gmail.com>
+ */
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { requireNativeComponent } from 'react-native'
-
-class TwilioVideoPreview extends Component {
-  static propTypes = {
-    /**
-     * Indicate if video feed is enabled.
-     */
-    enabled: PropTypes.bool.isRequired,
-    /**
-     * How the video stream should be scaled to fit its
-     * container.
-     */
-    scaleType: PropTypes.oneOf(['fit', 'fill'])
-  }
-
-  render () {
-    const scalesType = this.props.scaleType === 'fit' ? 1 : 2
-    return (
-      <NativeTwilioVideoPreview scalesType={scalesType} {...this.props}>
-        {this.props.children}
-      </NativeTwilioVideoPreview>
-    )
-  }
-}
-
-const NativeTwilioVideoPreview = requireNativeComponent(
-  'RNTwilioVideoPreview',
-  TwilioVideoPreview
-)
-
-module.exports = TwilioVideoPreview
+ import { requireNativeComponent, View } from 'react-native'
+ import React from 'react'
+ import PropTypes from 'prop-types'
+ 
+ const propTypes = {
+   ...View.propTypes,
+   /**
+    * How the video stream should be scaled to fit its
+    * container.
+    */
+   scaleType: PropTypes.oneOf(['fit', 'fill'])
+ }
+ 
+ class TwilioVideoPreview extends React.Component {
+   render () {
+     return <NativeTwilioVideoPreview {...this.props} />
+   }
+ }
+ 
+ TwilioVideoPreview.propTypes = propTypes
+ 
+ const NativeTwilioVideoPreview = requireNativeComponent(
+   'RNTwilioVideoPreview',
+   TwilioVideoPreview
+ )
+ 
+ module.exports = TwilioVideoPreview
