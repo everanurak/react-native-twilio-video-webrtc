e8f19ed51b3bb21c415adbe81669516f769d5f82
anurak <anurak@everapp.io>
2022-05-03 15:01:05 +0700

    brand new

--- src/TwilioVideoLocalView.android.js
+++ src/TwilioVideoLocalView.android.js
@@ -5,30 +5,33 @@
  *   Jonathan Chang <slycoder@gmail.com>
  */
 
- import { requireNativeComponent, View } from 'react-native'
- import React from 'react'
- import PropTypes from 'prop-types'
- 
- const propTypes = {
-   ...View.propTypes,
-   /**
-    * How the video stream should be scaled to fit its
-    * container.
-    */
-   scaleType: PropTypes.oneOf(['fit', 'fill'])
- }
- 
- class TwilioVideoPreview extends React.Component {
-   render () {
-     return <NativeTwilioVideoPreview {...this.props} />
-   }
- }
- 
- TwilioVideoPreview.propTypes = propTypes
- 
- const NativeTwilioVideoPreview = requireNativeComponent(
-   'RNTwilioVideoPreview',
-   TwilioVideoPreview
- )
- 
- module.exports = TwilioVideoPreview
+import { requireNativeComponent, View } from 'react-native'
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const propTypes = {
+  ...View.propTypes,
+  // Whether to apply Z ordering to this view.  Setting this to true will cause
+  // this view to appear above other Twilio Video views.
+  applyZOrder: PropTypes.bool,
+  /**
+   * How the video stream should be scaled to fit its
+   * container.
+   */
+  scaleType: PropTypes.oneOf(['fit', 'fill'])
+}
+
+class TwilioVideoPreview extends React.Component {
+  render () {
+    return <NativeTwilioVideoPreview {...this.props} />
+  }
+}
+
+TwilioVideoPreview.propTypes = propTypes
+
+const NativeTwilioVideoPreview = requireNativeComponent(
+  'RNTwilioVideoPreview',
+  TwilioVideoPreview
+)
+
+module.exports = TwilioVideoPreview
